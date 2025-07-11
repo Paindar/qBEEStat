1001c81e3e86b04a4de397cc23c95411f42a4b3d
Paindar <16387007+Paindar@users.noreply.github.com>
2025-07-11 19:10:56 +0800

    Revert "keep record for some seconds to peer reconnect."
    This reverts commit ef75bc415c0f56616c1ca6293af7deb23ee1f725.

--- src/base/statistics/tbl/tbl_peer_info.h
+++ src/base/statistics/tbl/tbl_peer_info.h
@@ -5,7 +5,6 @@
 #include <QDateTime>
 #include <QHash>
 #include <QList>
-#include <optional>
 
 namespace DB
 {
@@ -20,7 +19,6 @@ namespace DB
             qint64 uploadBytes;
             qint64 downloadBytes;
             QDateTime startTime;
-            std::optional<QDateTime> endingTime; // Time when the peer was last seen
         };
 
         static TblPeerInfo& instance(QSqlDatabase db)
@@ -35,15 +33,10 @@ namespace DB
         };
 
     public:
-        bool getPeer(const PrimaryKey& key, PeerInfo& result) const;
         bool getPeers(const QString& torrentHashId, QList<PeerInfo>& result) const;
-        bool getDeadPeers(int interval, QList<PeerInfo>& result) const;
         bool getAllPeers(QList<PeerInfo>& peers) const;
-        bool HasPeerInfo(const PrimaryKey& key) const;
         bool insertPeerInfo(const PrimaryKey& key, const QString& peerId, const qint64 uploadBytes, const qint64 downloadBytes, const QDateTime& startTime);
         bool updateTraffic(const PrimaryKey& key, const qint64 uploadBytes, const qint64 downloadBytes);
-        bool setPeerEndingTime(const PrimaryKey& key, const QDateTime& endingTime);
-        bool removePeerEndingTime(const PrimaryKey& key);
         bool deletePeerInfo(const PrimaryKey& key);
 
     private:
@@ -60,8 +53,6 @@ namespace DB
         QList<PeerInfo> fetchAllPeersFromDatabase(QString& error) const;
         bool insertPeerInfoToDatabase(const PrimaryKey& key, const QString& peerId, const qint64 uploadBytes, const qint64 downloadBytes, const QDateTime& startTime, QString& error);
         bool updatePeerTrafficInDatabase(const PrimaryKey& key, const qint64 uploadBytes, const qint64 downloadBytes, QString& error);
-        bool setPeerEndingTimeInDatabase(const PrimaryKey& key, const QDateTime& endingTime, QString& error);
-        bool removePeerEndingTimeFromDatabase(const PrimaryKey& key, QString& error);
         bool deletePeerInfoFromDatabase(const PrimaryKey& key, QString& error);
 
     private:
